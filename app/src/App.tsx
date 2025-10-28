29788d76baf29157547072459c2cded9e05e87fe
antaymard <antaymard@gmail.com>
2025-10-28 02:18:54 +0100

    convex init auth wip

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,12 @@
-import NodeEditor from "./components/node-editor/NodeEditor"
+import NodeEditor from "./components/node-editor/NodeEditor";
+import SignInPage from "./pages/SignInPage";
 
 function App() {
-
   return (
     <div className="h-screen w-screen p-3 ">
-      <NodeEditor />
+      <SignInPage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
